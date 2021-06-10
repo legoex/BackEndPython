b4fccdac640c9f7d082b52c8101e9148322c8a62
Oleg Korolenko <olegkorolenko.legoex@gmail.com>
2021-06-10 18:44:35 +0300

    Initial commit

---
+++ Models/Note.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class Note(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    content = Column(String)
---
+++ backend.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from Models.Note import Note, Base
+from fastapi.encoders import jsonable_encoder
+import os
+
+load_dotenv()
+engine = create_engine(os.getenv("DATABASE_URL"))
+Session = sessionmaker(bind=engine)
+
+
+def recreate_database():
+    Base.metadata.create_all(engine)
+
+
+recreate_database()
+
+app = FastAPI()
+
+
+@app.get("/notes")
+async def show():
+    session = Session()
+    notes = session.query(Note).all()
+    result = jsonable_encoder({
+        "notes": notes
+    })
+    return JSONResponse(status_code=200, content={
+        "status_code": 200,
+        "result": result
+    })
+
+
+@app.post("/notes")
+async def create_note(title: str, content: str):
+    session = Session()
+    note = Note(
+        title=title,
+        content=content
+    )
+    session.add(note)
+    session.commit()
+    session.close()
+    result = jsonable_encoder({
+        "note": note
+    })
+    return JSONResponse(status_code=200, content={
+        "status_code": 200,
+        "result": result
+    })
